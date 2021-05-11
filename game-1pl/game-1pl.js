3c9765babab62023348715446d301a64be50f556
rabbit <barateligeorge@yandex.ru>
2021-05-11 19:38:51 +0300

    change collision detection function

--- game-1pl/game-1pl.js
+++ game-1pl/game-1pl.js
@@ -163,7 +163,7 @@ function collisionDetection () {
     for (let r = 0; r < brickRowCount; r += 1) {
       let b = bricks[c][r];
       if (b.status === 1) {
-        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+        if (x + ballRadius > b.x && x - ballRadius < b.x + brickWidth && y + ballRadius > b.y && y - ballRadius < b.y + brickHeight) {
           dy = -dy;
           b.status = 0;
           score += 1;
